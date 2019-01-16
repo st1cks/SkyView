830267626871d537ad38c3e40b192693923c759a
BT_1N3_18 <rzhao3818@bths.edu>
2019-01-16 14:43:48 -0500

    Initial commit

---
+++ src/SkyView.java
@@ -0,0 +1,10 @@
+public class SkyView {
+    private double[][] view;
+
+    public SkyView(int numRows, int numCols, double[] scanned){
+
+    }
+    public double getAverage(int startRow, int endRow, int startCol, int endCol){
+
+    }
+}
